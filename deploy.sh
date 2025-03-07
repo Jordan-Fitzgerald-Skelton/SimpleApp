7947cacaa74701613bfa1051ad61adb4c857243b
Your Name <jordanfitzskelton45@gmail.com>
2025-03-07 11:23:08 +0000

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Update and install Node.js and npm
+sudo apt update && sudo apt install nodejs npm -y
+
+# Install pm2 which is a production process manager for Node.js
+sudo npm install -g pm2
+
+# Stop any instance of the application currently running under pm2
+pm2 stop example_app
+
+# Change directory into the folder where the application is downloaded
+cd SimpleApp/
+
+# Install application dependencies
+npm install
+
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+
+# Start the application with the process name "example_app" using pm2
+pm2 start ./bin/www --name example_app
